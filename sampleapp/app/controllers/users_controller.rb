56f4e699760837fbd0965568be67291bcb91865a
Huicheese <huihui.tiang@gmail.com>
2014-06-24 23:44:30 +0800

    Finish user edit, update, index, and destroy actions

--- sampleapp/app/controllers/users_controller.rb
+++ sampleapp/app/controllers/users_controller.rb
@@ -1,41 +1,72 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+ 
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
-  
-  def index
-  end
-
+ 
   def new
-  	@user = User.new
+    redirect_to(root_path) unless current_user?(nil)
+    @user = User.new
+    @title = "Sign up"
   end
-
+ 
   def create
-   @user = User.new(user_params)    # Not the final implementation!
-   if @user.save
-   	 flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
-   else
-     render 'new'
-   end
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = nil
+      @user.password_confirmation = nil
+      render 'new'
+    end
   end
-
-  # def update 
-  #   @user = User.find(user_params)
-  #   if @user.update_attributes(params[:user])
-  #     flash[:success] = "Account updated"
-  #     sign_in @user
-  #     redirect_to @user
-  #   else
-  #     render 'edit'
-  #   end
-  # end
-
+ 
+  def edit
+    @title = "Edit user"
+  end
+ 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+ 
   private
-
-   def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
-   end
-
+ 
+  def authenticate
+    deny_access unless signed_in?
+  end
+ 
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+ 
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- sampleapp/app/helpers/sessions_helper.rb
+++ sampleapp/app/helpers/sessions_helper.rb
@@ -7,6 +7,17 @@ module SessionsHelper
     	self.current_user = user
   	end
 
+    def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        sign_in user
+        redirect_back_or user
+      else
+        flash.now[:error] = 'Invalid email/password combination'
+        render 'new'
+      end
+    end
+
   	def signed_in?
     	!current_user.nil?
   	end
@@ -18,12 +29,25 @@ module SessionsHelper
   	def current_user
     	remember_token = User.digest(cookies[:remember_token])
     	@current_user ||= User.find_by(remember_token: remember_token)
-  	en
+  	end
+
+    def current_user?(user)
+      user == current_user
+    end
 
     def sign_out
       current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
       cookies.delete(:remember_token)
       self.current_user = nil
+    end
+ 
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
+    end
+
+    def store_location
+      session[:return_to] = request.url if request.get?
     end
 end
